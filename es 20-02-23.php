08470bedbe338c557d4ba081016aeb64e62eecdf
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-20 18:33:22 +0100

    corretto in group by la numero 3

--- es 20-02-23.php
+++ es 20-02-23.php
@@ -2,25 +2,23 @@
 -- #### Group By
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 -- ```sql
--- SELECT YEAR(enrolment_date), COUNT(*) 
--- FROM students 
+-- SELECT YEAR(enrolment_date), COUNT(*)
+-- FROM students
 -- GROUP BY YEAR(enrolment_date);
 -- ```
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 -- ```sql
--- SELECT office_address, COUNT(*) AS teachers 
--- FROM teachers 
+-- SELECT office_address, COUNT(*) AS teachers
+-- FROM teachers
 -- GROUP BY office_address;
 -- ```
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- ```sql
--- SELECT date, AVG(exam_student.vote) 
--- FROM exams 
--- JOIN exam_student 
--- ON exams.id = exam_student.exam_id 
--- GROUP BY date
+-- SELECT exam_id, AVG(vote)
+-- FROM exams_student
+-- GROUP BY exam_id;
 -- ```
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
@@ -31,19 +29,19 @@
 -- #### Join
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 -- ```sql
--- SELECT degrees.name, students.* 
--- FROM degrees 
--- JOIN students 
--- ON degrees.id = students.degree_id 
--- WHERE degrees.name 
+-- SELECT degrees.name, students.*
+-- FROM degrees
+-- JOIN students
+-- ON degrees.id = students.degree_id
+-- WHERE degrees.name
 -- LIKE 'Corso di Laurea in Economia';
 -- ```
 
 -- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
 -- ```sql
--- 	SELECT departments.name, degrees.* 
--- 	FROM departments 
--- 	JOIN degrees 
--- 	ON departments.id = degrees.department_id 
--- 	WHERE departments.name LIKE "%Dipartimento di Neuroscienze%" 
--- 	AND degrees.level LIKE "%magistrale%";
+-- SELECT departments.name, degrees.*
+-- FROM departments
+-- JOIN degrees
+-- ON departments.id = degrees.department_id
+-- WHERE departments.name LIKE "%Dipartimento di Neuroscienze%"
+-- AND degrees.level LIKE "%magistrale%";
